9d52a82efb6efa2debf9139f62f490ce887998f4
mpsmith414 <mpsmith414@gmail.com>
2014-11-05 11:31:59 -0400

    reads in command line

--- src/shellmain.c
+++ src/shellmain.c
@@ -3,22 +3,20 @@
 Basic core of the the shell
 
 Status:
-Displays the welcome message, then the prompt, then exits
+Displays the welcome message, then the prompt,
+allows user to type in a command and reads it, then exits
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#define BUFFER_SIZE 1200
+#define ARRAY_SIZE 100
+
 
 //Helper functions
-char* readCommandLine() {
-    char* cmdLine;
-    cmdLine = "test";
-    return cmdLine;
-}
-
-void parse(char* cmdLine) {
+void parse() {
 
 }
 
@@ -29,26 +27,26 @@ int main (void) {
     printf("\tYou are now running MysteryShell!\n");
     printf("====================================================\n\n");
 
-    char shellName[14] = "MysteryShell$ ";
-
     //main loop
     while (1) {
-        char *cmdLine;
+        char input[BUFFER_SIZE];
+        char *args[ARRAY_SIZE];
 
         // display a prompt
-        printf(shellName);
+        printf("MysteryShell$ ");
 
         //read in the command line
-        cmdLine = readCommandLine();
+        fgets(input, BUFFER_SIZE, stdin);
+        printf(input); //test that line is read properly
 
         //**record command in history list here**
 
         //parse command line
-        parse(cmdLine);
+        parse();
 
         //find the full pathname for the file
 
-        // create process to execute command
+        // create process - execute command
 
         //parent waits until child finishes executing command
         exit(0);
