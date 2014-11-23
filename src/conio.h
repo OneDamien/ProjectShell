f90332c1c658a1d019db0ee451cf8828def33072
Damien Robinson <damien.r@live.com>
2014-11-23 19:27:11 -0400

    Updated conio.h header

--- src/conio.h
+++ src/conio.h
@@ -16,16 +16,3 @@ int getch(void)
     return ch;
 }
 
-/* reads from keypress, echoes */
-int getche(void)
-{
-    struct termios oldattr, newattr;
-    int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
-    newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
-    ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
-    return ch;
-}
